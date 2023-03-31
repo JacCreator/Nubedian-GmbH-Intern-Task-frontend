79250be52bc69fa9c9f3e0953f6314334dcad86a
Kopyto <JacCreator@users.github.com>
2023-03-31 12:01:23 +0200

    Share code with Nubedian

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import EditCpu from "./pages/EditCpu";
 import Main from "./pages/Main";
-import CpuValidation from "./validations/CpuValidation";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/edit-cpu/:id" element={<EditCpu />} />
-        <Route path="/test" element={<CpuValidation />} />
       </Routes>
     </div>
   );
